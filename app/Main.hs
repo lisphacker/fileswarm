611691c92e1231e1b9b9415d0786cd50ff9acdfc
Gautham Ganapathy <gautham@lisphacker.org>
2017-11-15 22:18:12 +0000

    Modified cmd line options

--- app/Main.hs
+++ app/Main.hs
@@ -8,7 +8,9 @@ import Data.Text (pack, unpack)
 import Data.Bencoding (decode)
 import qualified Data.MetaInfo as MI
 
-data CommandLineOptions = CommandLineOptions { torrentFile :: Text }
+data CommandLineOptions = CommandLineOptions { torrentFile :: Text
+                                             , port        :: Int
+                                             } deriving (Show)
 
 
 run :: CommandLineOptions -> IO ()
@@ -21,4 +23,6 @@ run cmdLineOpts = do
 main :: IO ()
 main = execParser opts >>= run
   where opts = info parser mempty
-        parser = CommandLineOptions <$> argument text (metavar "TORRENT-FILE")
+        parser = CommandLineOptions
+          <$> pack <$> argument str ( metavar "TORRENT-FILE" <> help "Torrent file")
+          <*> argument auto (metavar "PORT" <> help "Port to listen to" <> showDefault <> value 6881)
